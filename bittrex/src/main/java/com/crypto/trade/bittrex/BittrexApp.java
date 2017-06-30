6d1cb28145c7ad140dc65d1309d0877cca44b354
Rolan Burykin <burykin.rolan@gmail.com>
2017-06-30 23:02:20 +0300

    Initial project commit.

---
+++ bittrex/src/main/java/com/crypto/trade/bittrex/BittrexApp.java
@@ -0,0 +1,14 @@
+package com.crypto.trade.bittrex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BittrexApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BittrexApp.class, args);
+    }
+
+
+}
